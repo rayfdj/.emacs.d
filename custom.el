46b9dc50cfba145fb5525ef4513e3891918e8151
RF <rayfdj@users.noreply.github.com>
2021-02-25 16:46:18 +0700

    Added org-roam to the list

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(swiper ivy company magit use-package f emacsql-sqlite3 doom-themes)))
+   '(org-roam swiper ivy company magit use-package f emacsql-sqlite3 doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
