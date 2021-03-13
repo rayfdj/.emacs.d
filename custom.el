624c11d80801a05d5510cd474d1339aace4548d0
RF <rayfdj@users.noreply.github.com>
2021-03-13 15:33:06 +0700

    Added projectile and racket-mode.

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(org-agenda-files
    '("~/Documents/org-roam/daily/2021-02-13.org" "~/Documents/org-roam/daily/2021-02-15.org" "~/Documents/org-roam/daily/2021-02-16.org" "~/Documents/org-roam/daily/2021-02-17.org" "~/Documents/org-roam/daily/2021-03-03.org" "~/Documents/org-roam/daily/2021-03-04.org" "~/Documents/org-roam/20210213115729-2021_tech_agenda.org" "~/Documents/org-roam/20210213133211-book_5_elements_of_effective_thinking.org" "~/Documents/org-roam/20210213133741-book_the_strategy_journal.org" "~/Documents/org-roam/20210215084210-ongoing_projects.org" "~/Documents/org-roam/20210215202203-project_flash.org" "~/Documents/org-roam/20210216115505-project_hinton.org" "~/Documents/org-roam/20210216154949-okr_framework.org" "~/Documents/org-roam/20210304143847-aws_contract_management.org"))
  '(package-selected-packages
-   '(multiple-cursors ivy-prescient prescient counsel org-roam swiper ivy company magit use-package f emacsql-sqlite3 doom-themes)))
+   '(exec-path-from-shell projectile racket-mode multiple-cursors ivy-prescient prescient counsel org-roam swiper ivy company magit use-package f emacsql-sqlite3 doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
